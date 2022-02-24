be8c40f77ccf3e3fd3cbdafb4904a77e98fc8464
김재훈 <wosl1006@naver.com>
2022-02-24 12:16:59 +0900

    220224java

---
+++ src/chap01/Hello.java
@@ -0,0 +1,10 @@
+package chap01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//콘솔에 출력하는 실행문
+		System.out.println("Hello,Welcome to the java world!");
+	} // end of main
+} //end of class
---
+++ src/chap01/RunStatementExample.java
@@ -0,0 +1,13 @@
+package chap01;
+
+public class RunStatementExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x =1;
+		int y =2;
+		int result=x+y;
+		System.out.println(x+"+"+y+"="+result);
+	}
+
+}
